38d2f0c235fa272957624b533e0a1085c08d20ba
Charles Stover <github@charlesstover.com>
2022-02-23 11:51:10 -0600

    exclude images from NYC config

--- packages/nyc-config/src/constants/exclude.cjs
+++ packages/nyc-config/src/constants/exclude.cjs
@@ -1,7 +1,9 @@
 module.exports = [
+  'src/**/*.{bmp,gif,ico,jpeg,jpg,png,psd,svg,tif,tiff,webp}',
   'src/**/*.d.ts',
   'src/**/*.e2e.ts',
   'src/**/*.spec.{js,jsx,ts,tsx}',
   'src/**/*.stories.{js,jsx,ts,tsx}',
   'src/**/*.test.{js,jsx,ts,tsx}',
+  'src/**/*.txt',
 ];
