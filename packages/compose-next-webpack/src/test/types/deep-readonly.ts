290b5acdf4b12d76eb50f80a13040b456f99f1fd
Charles Stover <github@charlesstover.com>
2022-09-25 15:04:02 -0700

    add `prepack` and `prepublish` to root

--- packages/compose-next-webpack/src/test/types/deep-readonly.ts
+++ packages/compose-next-webpack/src/test/types/deep-readonly.ts
@@ -1,5 +1,13 @@
-type DeepReadonly<T> = T extends object
-  ? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+// We need a type alias here, because we are looping over keys. We need the
+//   `Function` type here, because we are deliberately including all functions,
+//   as opposed to loosely typing our own function.
+// eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-type-alias
+type DeepReadonly<T> = T extends Function
+  ? T
+  : T extends object
+  ? {
+      readonly [K in keyof T]: DeepReadonly<T[K]>;
+    }
   : T;
 
 export default DeepReadonly;
