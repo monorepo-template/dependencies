83cde877b0ac14fa5c109c5ee6f457825c2648b8
Charles Stover <github@charlesstover.com>
2022-10-01 14:26:00 -0700

    skip impossible coverage test

--- packages/coverage/bin/coverage.ts
+++ packages/coverage/bin/coverage.ts
@@ -1,30 +1,5 @@
-import { existsSync } from 'fs';
-import { join } from 'path';
-import MISSING_CYPRESS_COVERAGE_DIRECTORY_ERROR from './constants/missing-cypress-coverage-directory-error';
-import MISSING_JEST_COVERAGE_DIRECTORY_ERROR from './constants/missing-jest-coverage-directory-error';
-import THRESHOLDS from './constants/thresholds';
-import mapCypressCoverageDirectoryToPaths from './utils/map-cypress-coverage-directory-to-paths';
-import coverage from '../src/index';
+import executeCoverage from './utils/execute-coverage';
 
 const CWD: string = process.cwd();
-const CYPRESS_COVERAGE_DIRECTORY: string = join(CWD, 'cypress', 'coverage');
-const JEST_COVERAGE_DIRECTORY: string = join(CWD, 'jest', 'coverage');
 
-if (!existsSync(CYPRESS_COVERAGE_DIRECTORY)) {
-  throw MISSING_CYPRESS_COVERAGE_DIRECTORY_ERROR;
-}
-
-if (!existsSync(JEST_COVERAGE_DIRECTORY)) {
-  throw MISSING_JEST_COVERAGE_DIRECTORY_ERROR;
-}
-
-const COVERAGE_PATHS: readonly string[] = [
-  JEST_COVERAGE_DIRECTORY,
-  ...mapCypressCoverageDirectoryToPaths(CYPRESS_COVERAGE_DIRECTORY),
-];
-
-await coverage({
-  paths: COVERAGE_PATHS,
-  thresholds: THRESHOLDS,
-  workingDirectory: CWD,
-});
+await executeCoverage(CWD);
