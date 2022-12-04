4dccc80f7205b46e15209a70a180b0c0f688fc7d
Charles Stover <github@charlesstover.com>
2022-12-04 01:53:37 -0800

    add support for scopeless workspace dependencies

--- packages/yarn-up-skip-workspace/src/utils/filter-options-by-keep-workspace-dependency.ts
+++ packages/yarn-up-skip-workspace/src/utils/filter-options-by-keep-workspace-dependency.ts
@@ -1,6 +1,6 @@
 const KEEP_WORKSPACE_DEPENDENCY =
   // eslint-disable-next-line no-control-regex
-  /^\x1B\[36m>\x1B\[39m \x1B\[36m\x1B\[4mKeep (@[\w-]+\/[\w-]+)@workspace:\^\x1B\[24m\x1B\[39m \x1B\[2m\(no changes\)\x1B\[22m$/;
+  /^\x1B\[36m>\x1B\[39m \x1B\[36m\x1B\[4mKeep ((?:@[\w-]+\/)?[\w-]+)@workspace:\^\x1B\[24m\x1B\[39m \x1B\[2m\(no changes\)\x1B\[22m$/;
 
 export default function filterOptionsByKeepWorkspaceDependency(
   option: string,
